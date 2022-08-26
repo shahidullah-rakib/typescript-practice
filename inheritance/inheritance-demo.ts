4d92992f0e24897d8e3216e78fad080436281d8b
shsidulla <shsidullarakib@gmail.com>
2022-08-26 12:22:09 +0600

    inheritance class

---
+++ inheritance/inheritance-demo.ts
@@ -0,0 +1,42 @@
+class User {
+    // properties, method, contractor
+    userName: string;
+    age: number;
+
+    constructor(userName: string, age: number) {
+        this.userName = userName;
+        this.age = age;
+    }
+
+    display() {
+        console.log(`userName: ${this.userName}, age: ${this.age}`)
+    }
+}
+
+class Student extends User {
+    studentId: number;
+
+    constructor(userName: string, age: number, studentId: number) {
+        super(userName, age)
+        this.studentId = studentId;
+    }
+
+    display() {
+        console.log(`userName: ${this.userName}, age: ${this.age}, id: ${this.studentId}`)
+    }
+
+}
+
+let student1 = new Student("rahim", 31, 101);
+student1.display();
+
+// //object
+// let user1 = new User("karim", 21)
+// user1.display();
+
+// let user2 = new User("karim", 21)
+// user1.display();
+
+export { };
+
+
