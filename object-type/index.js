ca55434e5418135a6a0085def4cc710cd8020267
shsidulla <shsidullarakib@gmail.com>
2022-08-26 11:16:33 +0600

    object type

---
+++ object-type/index.js
@@ -0,0 +1,14 @@
+var user1 = { userName: "rahim", userId: 101 };
+var user2;
+user2 = { userName: "Islam", userId: 102 };
+var user3;
+user3 = { userName: "karim", userId: 103 };
+var user4;
+user4 = [{ userName: "bakul", userId: 104 }, { userName: "tutul", userId: 105 }];
+// optional
+var user5;
+user5 = { userName: "Islam" };
+var users;
+users = [];
+users.push(user1, user2, user3, user4, user5);
+console.log(users);
---
+++ object-type/index.ts
@@ -0,0 +1,21 @@
+let user1 = { userName: "rahim", userId: 101 }
+
+let user2: { userName: string, userId: number }
+user2 = { userName: "Islam", userId: 102 }
+
+let user3: object;
+user3 = { userName: "karim", userId: 103 }
+
+let user4: object[];
+user4 = [{ userName: "bakul", userId: 104 }, { userName: "tutul", userId: 105 }]
+
+// optional
+let user5: { userName: string, userId?: number }
+user5 = { userName: "Islam" }
+
+let users: object[];
+users = [];
+
+users.push(user1, user2, user3, user4, user5)
+
+console.log(users)
